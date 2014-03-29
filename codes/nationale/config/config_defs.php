3bbbcceda6c479647185e2f8c3e592b80409aeae
patrickpollet <pp@patrickpollet.net>
2014-03-29 16:49:11 +0100

    ménage code spécifique aux nationales

--- codes/nationale/config/config_defs.php
+++ codes/nationale/config/config_defs.php
@@ -1,8 +1,8 @@
 <?php
-if ($CFG->W3C_strict) {
 
 	function maj_defs_telechargements($node) {
 		global $CFG;
+/***************************************************************************************************		
 		//rev 940 la nationale n'est peut-�tre pas a jour
 		//donc aller chercher la derniere et l'afficher dans le template config.html'
 		$version_svn = get_version_svn();
@@ -26,7 +26,7 @@ if ($CFG->W3C_strict) {
         	 $node->addItem(cree_item_lien(traduction('telechargement_maj').' '.$version_svn,
 					$CFG->chemin .'/codes/nationale/telecharger_pf.php?quoi=maj',true));
 		}
-
+********************************************************************************************************/
 
         //rev 1009
         if (is_admin()) {
@@ -58,63 +58,5 @@ if ($CFG->W3C_strict) {
 
 	}
 
-} else {
-	// code p�rim� a partir 1.5 revision 963 (a virer en 1.6)
-	 /**
-  * CE SCRIPT NE DEVRAIT PLUS ETRE UTILISE APRES LA VERSION 1.5 963 (rev svn 990)
-  * ET SERA SUPPRIME en version 1.6
-  */
 
-	function maj_defs_telechargements($tpl) {
-		global $CFG;
-		//rev 940 la nationale n'est peut-�tre pas a jour
-		//donc aller chercher la derniere et l'afficher dans le template config.html'
-		$CFG->version_svn = get_version_svn();
-
-		if (a_capacite("plct") && a_capacite("plpt")) {
-			$tpl->newblock("telpc");
-
-			if ($CFG->telechargement_pf)
-				$tpl->assignURL("url_telpc", $CFG->chemin .
-				"/codes/nationale/telecharger_pf.php?quoi=pc");
-			else
-				$tpl->assignURL("url_telpc", "$CFG->chemin_commun/legende.php?quoi=non_implemente");
-		}
-		if (a_capacite("plct")) {
-			$tpl->newblock("telc");
-			if ($CFG->telechargement_pf)
-				$tpl->assignURL("url_telpc", $CFG->chemin .
-				"/codes/nationale/telecharger_pf.php?quoi=c");
-			else
-				$tpl->assignURL("url_telpc", "$CFG->chemin_commun/legende.php?quoi=non_implemente");
-		}
-		if (a_capacite("plpt")) {
-			$tpl->newblock("telp");
-			if ($CFG->telechargement_pf)
-				$tpl->assignURL("url_telpc", $CFG->chemin .
-				"/codes/nationale/telecharger_pf.php?quoi=p");
-			else
-				$tpl->assignURL("url_telpc", "$CFG->chemin_commun/legende.php?quoi=non_implemente");
-		}
-		if (a_capacite("plct") || a_capacite("plpt")) {
-			$tpl->newblock("telmaj");
-			if ($CFG->telechargement_pf)
-				$tpl->assignURL("url_telmaj", $CFG->chemin .
-				"/codes/nationale/telecharger_pf.php?quoi=maj");
-			else
-				$tpl->assignURL("url_telmaj", "$CFG->chemin_commun/legende.php?quoi=non_implemente");
-		}
-	}
-
-	function maj_defs_avancees($tpl) {
-		global $CFG;
-
-		$tpl->assign("url_gestion_etabs", $CFG->chemin . "/codes/nationale/etablissements/liste.php");
-		$tpl->assign("url_gestion_familles", $CFG->chemin . "/codes/nationale/familles/liste.php");
-		$tpl->assign("url_gestion_refs", $CFG->chemin . "/codes/nationale/referentiels/liste.php");
-		$tpl->assign("url_gestion_alineas", $CFG->chemin . "/codes/nationale/alineas/liste.php");
-
-	}
-
-}
 ?>
