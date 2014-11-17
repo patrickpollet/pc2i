1f85344608cc36490bd89cab3b77834fc15739ce
patrickpollet <pp@patrickpollet.net>
2014-11-17 19:08:31 +0100

    un fichier de langue locale eventuel n'était plus pris en compte

--- codes/locale/fr_utf8.php
+++ codes/locale/fr_utf8.php
@@ -1,23 +1,24 @@
 <?php
 /**
  * @author Patrick Pollet
- * @version $Id: fr.php 822 2009-05-28 16:04:32Z ppollet $
+ * @version $Id: fr.php 822 2014-11-17 16:04:32Z ppollet $
  * @license http://www.gnu.org/copyleft/gpl.html GNU Public License
  * @package c2ipf
  */
 
  /**
-  * exemple de personnalisation de la traduction
+  * exemples de personnalisation de la traduction
   *
   */
 
 
- $textes_langues["bouton_tester"]="tester/ retester ...";
+ //$textes_langues["bouton_tester"]="tester/ retester ...";
 
-$textes_langues["texte_bienvenue"] =
+/*
+ $textes_langues["texte_bienvenue"] =
 "<div class='information'>Bienvenue sur la plate-forme de certification et positionnement pour le C2i® niveau 1 <br/>".
 "hebergée au Centre Informatique du Premier Cycle de l'INSA de Lyon</div>";
-
+*/
 
 
 
